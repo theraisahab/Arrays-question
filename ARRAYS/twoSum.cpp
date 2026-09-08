f3bd0e2d42e0b5db727c8047446eb9e589a2e748
Avnish Rai <115795192+theraisahab@users.noreply.github.com>
2026-09-08 21:27:49 +0530

    Add files via upload

---
+++ ARRAYS/twoSum.cpp
@@ -0,0 +1,70 @@
+// #include<bits/stdc++.h>
+
+// using namespace std;
+// vector<int> twoSum(vector<int> &arr, int target){
+//     map<int,int> mpp;
+//     int n = arr.size();
+//     for(int i=0; i<n; i++){
+//         int temp = arr[i];
+//         int more = target - temp;
+//         if(mpp.find(more) != mpp.end()){
+//             return {mpp[more], i};
+//         }
+//         mpp[temp] = i;
+//     }
+//     return {-1,-1};
+// }
+// int main(){
+//     vector<int> arr = {2,7,11,15};
+//     int target = 14;
+//      twoSum(arr, target);
+    
+//     return 0;
+// }
+
+// #include<bits/stdc++.h>
+
+// using namespace std;
+
+// vector<int> twoSum(vector<int> &arr, int target){
+//     map<int, int> mpp;
+
+//     int n = arr.size();
+//     for(int i=0; i<n; i++){
+//         int temp = arr[i];
+//         int more = target - temp;
+//         if(mpp.find(more) != mpp.end()){
+//             return {mpp[more], i};
+//         }
+//     }
+//     return {-1,-1};
+// }
+
+
+
+// int main(){
+//     vector<int> arr = {2,7,11,15};
+//     int target = 14;
+//     twoSum(arr, target);
+//     return 0;
+// }
+
+#include<bits/stdc++.h>
+
+
+using namespace std;
+
+
+vector<int> twoSum(vector<int> &arr, int target){
+    map<int, int> mpp;
+    for(int i=0; i<n; i++)
+}
+
+int main(){
+    vector<int> arr= {2,7,11,15};
+    int target = 14;
+    twoSum(arr, target);
+    
+    return 0;
+
+}
